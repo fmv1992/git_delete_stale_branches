b7e28d068158c30df564c594d30218608b7c0088
Felipe M. Vieira <felipev@telnyx.com>
2025-02-28 14:55:27 -0300

    updated data

--- other/test/test_library/test_01.sh
+++ other/test/test_library/test_01.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 
 source "${REFERENCE_DIR}/usr/local/lib/git_delete_stale_branches/git_delete_stale_branches"
 
-_validate_number_of_arguments one two three 3
-_validate_number_of_arguments 'one two three' 1
+validate_number_of_arguments 3 'error' one two three
+validate_number_of_arguments 1 'error' 'one two three'
 
-! _validate_number_of_arguments one two three 2
-! _validate_number_of_arguments one two three 4
-! _validate_number_of_arguments 'one two three' 3
+! validate_number_of_arguments 2 'error' one two three
+! validate_number_of_arguments 4 'error' one two three
+! validate_number_of_arguments 3 'error' 'one two three'
 
 # vim: set filetype=sh fileformat=unix nowrap:
