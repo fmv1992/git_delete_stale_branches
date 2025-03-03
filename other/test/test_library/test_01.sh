0c534fd4ba1e279ef5c844f484d1c43d1fe3cca4
Felipe M. Vieira <fmv1992@gmail.com>
2025-03-03 20:32:47 -0300

    NOJIRA-119: updated documentation; added migration script

--- other/test/test_library/test_01.sh
+++ other/test/test_library/test_01.sh
@@ -13,7 +13,7 @@ validate_number_of_arguments 1 'one two three'
 ! validate_number_of_arguments 3 'one two three'
 
 function function_test_example_sum() {
-    validate_number_of_arguments 2 ${@}
+    validate_number_of_arguments 2 ${@} || return $?
     local a=${1}
     local b=${2}
     echo $((a + b))
@@ -21,6 +21,8 @@ function function_test_example_sum() {
 
 (($(function_test_example_sum 1 2) == 3))
 (($(function_test_example_sum 57 2) == 59))
+
+! function_test_example_sum 1
 
 {
     # These generate error messages.
